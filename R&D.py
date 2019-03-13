b0f5f2c610ed11846926f5515d8e1a5e1bf13599
CrazyCipherGamer5827 <raja.sell5827@gmail.com>
2019-03-13 19:52:50 -0400

    Fix everything and add .gitignore

--- R&D.py
+++ R&D.py
@@ -1,6 +1,3 @@
-#switcher = {"car": carFunction(), "boat": boatFunction(), "airplane": airFunction()}
-
-
 class Vehicle():
 	def __init__(self, quantity, speed, ModelNumber, AmountGas, KPL, NumPassengers):
 		self.quantity = quantity
@@ -11,63 +8,63 @@ class Vehicle():
 		self.NumPassengers = NumPassengers
 
 
-	def speed(x):
-		return x * (18 / 5)
+	def getspeed(self):
+		return self.speed * (18.0 / 5.0)
 		
-	def AmountGas(x):
-		return (50 - (2 * x))
+	def getAmountGas(self):
+		return self.AmountGas
 
-	def KPL(x):
-		return x
+	def getKPL(self):
+		return self.KPL
 
-	def getQuantity(x):
-		return x
+	def getQuantity(self):
+		return self.quantity
 
-	def getModelNumber(x):
-		return x
+	def getModelNumber(self):
+		return self.ModelNumber
 
-	def NumPassengers(x):
-		return x
+	def getNumPassengers(self):
+		return self.NumPassengers
 
 class Boat(Vehicle):
-	def __init__(self, isAfloat):
-		self.ModelNumber = ModelNumber
-		isAfloat = True
+	def __init__(self, quantity, speed, ModelNumber, AmountGas, KPL, NumPassengers, isAfloat):
+		Vehicle(quantity, speed, ModelNumber, AmountGas, KPL, NumPassengers)
+		self.isAfloat = isAfloat
 
-	def NumPassengers(x):
-		return x + 4
+	def getNumPassengers(self):
+		return Vehicle.getNumPassengers(self) + 4
 
 class Car(Vehicle):
-	def __init__(self, ):
+	def __init__(self, quantity, speed, ModelNumber, AmountGas, KPL, NumPassengers):
+		Vehicle(quantity, speed, ModelNumber, AmountGas, KPL, NumPassengers)
+	
+	def getNumPassengers(self):
+		return Vehicle.getNumPassengers(self) - 2
 
+def carFunction():
+	gas = float(input("How much gas would like in your car (in liters)?: "))
+	passengers = int(input("Please enter the number of passengers traveling in the car with you today. Please note that you may only have a maximum of 4 passengers."))
+	CarSpeed = float(input("Please enter the speed of the car in miles per hour (this speed will be converted into km/hr)."))
+	car = Car(1, CarSpeed, 1001, gas, 50, passengers)
 
-	def NumPassengers(x):
-		return x - 2
+def boatFunction():
+	gas = float(input("How much gas would you like in your boat (in liters)?: "))
+	passengers = int(input("Please enter the number of passengers traveling in the boat with you today. Please note that you may only have a maximum of 10 passengers."))
+	BoatSpeed = float(input("Please enter the speed of the boat in miles per hour (this speed will be converted into km/hr)."))
+	boat = Boat(1, BoatSpeed, 1001, gas, 15, passengers, True)
 
-class Main(Vehicle):
-		vehicle = str(input("Welcome to Alan Donovan Motors Testing Department! Please enter what type of vehicle you will be testing today."))
+switcher = {"car": carFunction, "boat": boatFunction}
 
-		if vehicle == "car":
-			car = Vehicle()
-			gas = 50
-			print(str("You currently have " + str(gas) + " liters left in the tank."))
-			passengers = str(input("Please enter the number of passengers traveling in the car with you today. Please note that you may only have a maximum of 4 passengers."))
-			CarSpeed = float(input("Please enter the speed of the car in miles per hour (this speed will be converted into km/hr)."))
-			Vehicle.speed(CarSpeed)
-			print("There are " + str(gas) + " left in the tank of the car, there are " + str(passengers) + " passengers traveling with you in the car, and you are traveling at " + str(speed) + " km/hr.")
+vehicle = str(input("Welcome to Alan Donovan Motors Testing Department! Please enter what type of vehicle you will be testing today."))
+if vehicle == "car":
+	switcher.get(vehicle)()
 
-		elif vehicle == "boat":
-			gas = 50
-			print(str("You currently have " + str(gas) + " liters left in the tank."))
-			passengers = str(input("Please enter the number of passengers traveling in the boat with you today. Please note that you may only have a maximum of 10 passengers."))
-			BoatSpeed = float(input("Please enter the speed of the boat in miles per hour (this speed will be converted into km/hr)."))
-			speed(BoatSpeed)
-			print("There are " + str(gas) + " left in the tank of the boat, there are " + str(passengers) + " passengers traveling with you in the boat, and you are traveling at " + str(speed) + " km/hr.")
+elif vehicle == "boat":
+	switcher.get(vehicle)()
 
-		elif vehicle == "airplane":
-			gas = 50
-			print(str("You currently have " + str(gas) + " liters left in the tank."))
-			passengers = str(input("Please enter the number of passengers traveling in the airplane with you today. Please note that you may only have a maximum of 25 passengers."))
-			AirplaneSpeed = float(input("Please enter the speed of the airplane in miles per hour (this speed will be converted into km/hr)."))
-			Vehicle.speed(AirplaneSpeed)
-			print("There are " + str(gas) + " left in the tank of the airplane, there are " + str(passengers) + " passengers traveling with you in the airplane, and you are traveling at " + str(speed) + " km/hr.")
+# elif vehicle == "airplane":
+	# gas = 50
+	# print(str("You currently have " + str(gas) + " liters left in the tank."))
+	# passengers = str(input("Please enter the number of passengers traveling in the airplane with you today. Please note that you may only have a maximum of 25 passengers."))
+	# AirplaneSpeed = float(input("Please enter the speed of the airplane in miles per hour (this speed will be converted into km/hr)."))
+	# print("There are " + str(gas) + " left in the tank of the airplane, there are " + str(passengers) + " passengers traveling with you in the airplane, and you are traveling at " + str(speed) + " km/hr.")
